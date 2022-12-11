3fa7d9736bf5df89e1f3c3dbce6d7a66bcf02688
Julia <66594323+vmjulia@users.noreply.github.com>
2022-12-11 11:58:16 +0100

    update

--- crowdsource.py
+++ crowdsource.py
@@ -1,27 +1,37 @@
 import pandas as pd
 import os
-from rdflib import Namespace, URIRef
+from rdflib import Namespace, URIRef, Literal, XSD
+import rdflib
 import re
+from graph import Graph
+import pickle
+from nltk.metrics import agreement
+
 
 class CrowdSource:
-    def __init__(self):
-        dirname = os.path.dirname(__file__)
-        self.crowd_data = pd.read_csv(os.path.join(dirname, '../../../../data/ddis/filtered_crowd_data_ISO_dates.csv'))
-        print("CrowdSource initialized")
+    def __init__(self, createNew = False):
+        self.crowd_graph = None
+        self.crowd_data = pd.read_csv('data/crowd_data.tsv', sep='\t', header=0)
+        if createNew:
+            self.compute_kappa()
+            graph =  Graph(False)          
+            self.create_difference_graph(graph.graph)
+            self.dump()
+        else:  
+            self.compute_kappa()     
+            #with open('data/crowd_graph.pkl', 'rb') as file:
+            #    self.crowd_graph = pickle.load(file)
 
-    def merge_data(self, graph):
+    def create_difference_graph(self, graph):
+        self.crowd_graph  = rdflib.Graph()
         WD = Namespace('http://www.wikidata.org/entity/')
         WDT = Namespace('http://www.wikidata.org/prop/direct/')
         DDIS = Namespace('http://ddis.ch/atai/')
         grouped = self.crowd_data.groupby('HITId')
         for hitId, group in grouped:
             hit = group.iloc[0]
-            votes = group['AnswerLabel'].value_counts().to_frame('count').reset_index()
-            # majority_vote = votes['index'].iloc[0]
-            # if majority_vote == 'CORRECT':
-            # merge crowd data with graph
             s = URIRef(WD[re.sub("wd:", "", hit['Input1ID'])])
-
+            
             p_value = hit['Input2ID']
             if re.search("ddis:", p_value):
                 p = URIRef(DDIS[re.sub("ddis:", "", p_value)])
@@ -36,15 +46,35 @@ class CrowdSource:
             else:
                 o = Literal(o_value)
             if not (s, p, o) in graph:
-                graph.add((s, p, o))
+                self.crowd_graph .add((s, p, o))
+                print("added", s, p , o)
 
+    def dump(self, ):
+        with open('data/crowd_graph.pkl', 'wb') as file:
+            pickle.dump( self.crowd_graph , file)            
+    
+    def compute_kappa(self):
+        batches = self.crowd_data.groupby('HITTypeId')
+        for batchId, batch in batches:
+            data = []
+            grouped = batch.groupby('HITId')
+            for hitId, group in grouped:
+                index = 0
+                for idx, row in group.iterrows():
+                    index += 1
+                    data.append((f"Worker_{index}", str(hitId), row['AnswerID']))
+            task = agreement.AnnotationTask(data=data)
+            print("Fleiss Kappa:", task.multi_kappa())
+            self.crowd_data.loc[self.crowd_data['HITTypeId'] == batchId, 'kappa'] = task.multi_kappa()
+
+       
     def find_answer(self, s, p, o):
         df = self.crowd_data
         crowd_answers = df.loc[(df['Input1ID'] == s) & (df["Input2ID"] == p) & (df["Input3ID"] == o)]
         if crowd_answers.empty:
             return None
         else:
-            kappa = crowd_answers['Kappa'].iloc[0]
+            kappa = crowd_answers['kappa'].iloc[0]
             votes = crowd_answers['AnswerLabel'].value_counts().to_frame('count').reset_index()
             if votes.loc[votes['index'] == 'CORRECT', 'count'].values:
                 support_votes = votes.loc[votes['index'] == 'CORRECT', 'count'].values[0]
@@ -56,3 +86,11 @@ class CrowdSource:
             else:
                 reject_votes = 0
             return support_votes, reject_votes, round(kappa, 2)
+        
+if __name__ == '__main__':
+    cs =  CrowdSource(False)
+    s =  "wd:Q4335275"
+    p =  "wdt:P520"
+    o =  "wd:Q52382294"
+    res = cs.find_answer(s, p, o)
+    print(res)
--- embedding.py
+++ embedding.py
@@ -160,17 +160,70 @@ class EmbeddingService:
                 return False, "No, Thats wrong "
         else:
             print("heress")
-            return False, "Unfortunately, seems I dont have this in my embeddings, could you try to rephrase, maybe then I can find...."       
+            return False, "Unfortunately, seems I dont have this in my embeddings, could you try to rephrase, maybe then I can find...."  
+        
+    def answer_general_question(self, s, p, cardinality):
+        sub = self.WD[s[len("wd:"):]]
+        if re.search("ddis:", p):
+            pred = self.DDIS[p[len("ddis:"):]]
+        else:
+            pred = self.WDT[p[len("wdt:"):]]
+       # obj = self.WD[o[len("wd:"):]] if re.search("wd:", o) else o
+       
+        if sub in self.ent2id and pred in self.rel2id:
+             # given head and rel find tail
+            head = self.entity_emb[self.ent2id[sub]]
+            pred = self.relation_emb[self.rel2id[pred]]
+            
+            lhs = head + pred
+            dist1 = pairwise_distances(lhs.reshape(1, -1), self.entity_emb).reshape(-1)
+            most_likely = dist1.argsort()
+            if cardinality == "Single" or cardinality == "Not Found":
+                idx1 = most_likely[0]
+                idx1 = [idx1]
+            else:
+                idx1 = most_likely[:5]
+            
+            # given tail and rel find head
+            lhs = head - pred
+            dist2 = pairwise_distances(lhs.reshape(1, -1), self.entity_emb).reshape(-1)
+            most_likely = dist2.argsort()
+            if cardinality == "Single" or cardinality == "Not Found":
+                idx2 = most_likely[0]
+                idx2 = [idx2]
+            else:
+                idx2 = most_likely[:5]
+            
+            if len(idx1) == 1 and len(idx2)==1: 
+                if dist1[idx1[0]]<dist2[idx2[0]]:
+                    return  self.id2ent[idx1[0]]
+                else: 
+                    return  self.id2ent[idx2[0]]
+            
+            else: 
+                if dist1[idx1[0]]<dist2[idx2[0]]:
+                    res = []
+                    for el in idx1:
+                        res.append(self.id2ent[el])
+                    return  res
+                else: 
+                    res = []
+                    for el in idx2:
+                        res.append(self.id2ent[el])
+                    return  res
+        else:
+            return ("Unfortunately, seems I dont have this in my embeddings, could you try to rephrase, maybe then I can find....")
+                 
         
 if __name__ == '__main__':
     embed = EmbeddingService()
     graph =  Graph(False)
    
-    o = "wd:Q18665339"
-    s = "wd:Q235347"
+    s = "wd:Q18665339"
+    o = "wd:Q235347"
     p = "wdt:P1657"
-    o_label = "NC-17"
-    s_label = "Weathering with You"
+    s_label = "NC-17"
+    o_label = "Weathering with You"
     p_label = "MPAA film rating"
     
     
