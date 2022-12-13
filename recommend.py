5af69cba81f6b1f0adcefb7e785bbab9a2867e02
Julia <66594323+vmjulia@users.noreply.github.com>
2022-12-13 03:08:24 +0100

    add recommender

--- recommend.py
+++ recommend.py
@@ -13,20 +13,25 @@ class Recommender():
         self.graph = graph
         self.RDFS = rdflib.namespace.RDFS
         self.WD = rdflib.Namespace('http://www.wikidata.org/entity/')
-        self.embeddings = np.load('data/entity_embeds.npy')
         self.embeddingService =  embeddingService
         
         
     def findSimilarEmbeddings(self, matches):
         entity =  self.graph.entityToURI(matches[0])
         entity  = self.WD[entity[len("wd:"):]]  
-        entity = self.embeddingService.entity_emb[self.ent2id[entity]]
-        dist = pairwise_distances(self.embeddingService.entity_emb[entity].reshape(1, -1), self.embeddingService.entity_emb).reshape(-1)
+        entity = self.embeddingService.entity_emb[self.embeddingService.ent2id[entity]]
+        
+        dist = pairwise_distances(entity.reshape(1, -1), self.embeddingService.entity_emb).reshape(-1)
         sim = dist.argsort()[1:20]        
         return sim
+    
+    # peak 20 most similar from embeddings and select same genre and or same director
+    # if this returns none then just same genre and or director actor ect
     
 if __name__ == '__main__':
     embed = EmbeddingService()
     graph =  Graph(False)
     r = Recommender(graph,  embed)
-    res = r.findSimilarEmbeddings("Star Wars")
+    matches = ["wd:Q235347"]
+    res = r.findSimilarEmbeddings(matches)
+    print(res)
